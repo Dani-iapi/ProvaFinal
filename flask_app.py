98d39c8160717b5a947f412424895676d67467d1
Dani-iapi <dani_iapi@yahoo.com.br>
2020-05-26 18:17:50 -0300

    primeiro

--- flask_app.py
+++ flask_app.py
@@ -1,78 +1,57 @@
-from flask import Flask, render_template, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
-# from pytz import timezone
+from flask import Flask, render_template
+#from flask_sqlalchemy import SQLAlchemy
+#from datetime import datetime
 
 app = Flask(__name__)
 
 ##### Configurar o banco de dados
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bancoDeDados.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bancoDeDados.db'
+#app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+#db = SQLAlchemy(app)
 #####
 
 ##### Criar o modelo para o registro do usuario
 # class Usuario(db.Model):
 #     codigo = 
 
-class Apontamento(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    nome_usuario = db.Column(db.String(25), nullable=True)
-    hora = db.Column(db.DateTime, default=datetime.now)
-    # hora = db.Column(db.DateTime, default=datetime.now().astimezone(timezone('America/Sao_Paulo')))
+#class Apontamento(db.Model):
+#    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+#    nome_usuario = db.Column(db.String(25), nullable=True)
+#   hora = db.Column(db.DateTime, default=datetime.now)
 
-# class Usuario(db.Model)
-#     login = 
-#     email = 
+#db.create_all()
 
-    @property
-    def serializar(self):
-        return {
-            'id': self.id,
-            'nome_usuario': self.nome_usuario,
-            'hora': str(self.hora)[11:16]
-        }
 
-# db.create_all()
+
+
+# @app.route('/api/funcionarios', methods=['GET', 'POST'])
+# def funcionarios():
+#     usuario = Apontamento(nome_usuario=)
+#     db.session.add(usuario)
+#     db.session.commit()
+
+
 
 @app.route('/inicio')
 @app.route('/')
-def mostra_inicio():
-    return render_template('index.html')
+def inicio():
+    return render_template('inicio.html')
 
-@app.route('/funcionarios', methods=['GET', 'POST'])
-def funcionarios():
-    if request.method == 'POST':
-        print(request.form['nome']) # a pessoa digitou
-        usuario = Apontamento(nome_usuario=request.form['nome'])
-        db.session.add(usuario)
-        db.session.commit()
-        return render_template('index.html')
-
-    if request.method == 'GET':
-        ultimos10 = Apontamento.query.order_by(-Apontamento.id).limit(10).all()
-        print(ultimos10)
-        j = jsonify(funcionarios=[i.serializar for i in ultimos10])
-        print(j)
-        return j
-        
-
-@app.route('/contato')
-def contato():
-    return render_template('contato.html')
+@app.route('/cursos')
+def cursos():
+    return render_template('Cursos.html')
 
 @app.route('/sobre')
 def sobre():
-    return render_template('sobre.html')
+    return render_template('Sobre.html')
 
+@app.route('/contato')
+def contato():
+    return render_template('Contato.html')
 
-# @app.route('/login')
-# def login():
-#     return render_template('login.html')
-
-
-
-
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 ## Para rodar o projeto em desenvolvimento
 
